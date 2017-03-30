a1c5740a039d8ec4a795be668fb1b71c0265e9b4
Aaron Sly <hello@aaronsly.com>
2017-03-30 12:19:49 +0100

    Update link-things.php
    Added Snippet to display a list of child pages sharing the same parent and includes the parent page link

--- page-snippets/link-things.php
+++ page-snippets/link-things.php
@@ -1,7 +1,7 @@
 /***************************************
-*Snippet to display a list of child pages sharing the same parent
-*is_subpage() function can be found here:
-*https://github.com/AaronSly/Wordpress-Snippets/Wordpress-Snippets/functions-file-stuff/page-parent-child-checks.php
+* Snippet to display a list of child pages sharing the same parent
+* is_subpage() function can be found here:
+* https://github.com/AaronSly/Wordpress-Snippets/blob/master/functions-file-stuff/page-parent-child-checks.php
 *****************************************
   <?php
   $parentID = is_subpage();
@@ -19,3 +19,18 @@
 			</ul>
 		</div>
   <?php endif;?>
+
+/***************************************
+* Snippet to display a list of child pages sharing the same parent and includes the parent page link
+* is_subpage() function can be found here:
+* https://github.com/AaronSly/Wordpress-Snippets/blob/master/functions-file-stuff/page-parent-child-checks.php
+*****************************************
+<?php $parentID = is_subpage();
+if($parentID) :
+$args   = array( 'child_of' => $parentID, 'sort_column' =>'menu_order', 'depth' => 0, 'title_li' => '' );
+?>
+<ul class="nav-right">	
+	<li><a href="<?php the_permalink($parentID);?>"><?php echo get_the_title($parentID);?></a></li>
+	<?php wp_list_pages( $args );?>
+</ul>
+<?php endif;?>
