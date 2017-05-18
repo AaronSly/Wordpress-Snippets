d0cafc9040a173b0a9205d14a2182bf528564225
Aaron Sly <hello@aaronsly.com>
2017-05-18 15:42:30 +0100

    added jumpto

--- page-snippets/link-things.php
+++ page-snippets/link-things.php
@@ -34,3 +34,32 @@ $args   = array( 'child_of' => $parentID, 'sort_column' =>'menu_order', 'depth' 
 	<?php wp_list_pages( $args );?>
 </ul>
 <?php endif;?>
+
+/***************************************
+* Snippet to create a jump to select box in a wordpress page - Ideally used on mobile devices
+* is_subpage() function can be found here:
+* https://github.com/AaronSly/Wordpress-Snippets/blob/master/functions-file-stuff/page-parent-child-checks.php
+*****************************************
+<?php $parentID = is_subpage();
+	if(!empty($parentID)) : ?>
+	
+	<div class="jump-to clearfix visible-sm visible-xs">
+	<?php $args = array(
+			'depth'        => 1,
+			'sort_column'  => 'menu_order',
+			'child_of'     => $parentID,
+			'echo' => 0,
+			'selected' => $post->ID,
+			'id' => 'jumpto'
+		);?>
+
+		<form action="<?php bloginfo('url'); ?>" method="get">
+			<label for="jumpto">Jump to: </label>
+			<?php
+			$select = wp_dropdown_pages($args);
+
+			echo str_replace('<select ', '<select onchange="this.form.submit()" ', $select);
+			?>
+		</form>			
+	</div>
+<?php endif;?>
